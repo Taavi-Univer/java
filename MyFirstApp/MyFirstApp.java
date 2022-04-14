cab3b70c288616e0ae32f4f4b05bd964643dae82
Taavi-Univer <taavi.univer@gmail.com>
2022-04-14 16:26:00 +0300

    MyFirstApp

---
+++ MyFirstApp/MyFirstApp.java
@@ -0,0 +1,12 @@
+package MyFirstApp;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Testing...");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i is now " + i);
+        }
+    }
+}

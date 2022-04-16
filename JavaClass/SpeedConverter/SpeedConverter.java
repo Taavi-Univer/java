ce6f9ba38608e987a16b17cd7b3561e171f9c72e
Taavi-Univer <taavi.univer@gmail.com>
2022-04-16 20:11:38 +0300

    SpeedConverter

---
+++ JavaClass/SpeedConverter/SpeedConverter.java
@@ -0,0 +1,27 @@
+package JavaClass.SpeedConverter;
+
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+
+    public static void main(String[] args) {
+        // double n = 1.5;
+        // long r = Math.round(n);
+        // System.out.println(r);
+        printConversion(-5.6);
+    }
+}

5d0d02131d171041ff7d8fdea3e9c9f7b17cf79a
Taavi-Univer <taavi.univer@gmail.com>
2022-04-17 13:37:08 +0300

    Delete MyFirstApp directory

--- MyFirstApp/GuessingGame/GuessingGame.java
+++
@@ -1,8 +0,0 @@
-package MyFirstApp.GuessingGame;
-
-public class GuessingGame {
-    public static void main(String[] args) {
-        GuessGame game = new GuessGame();
-        game.startGame();
-    }
-}

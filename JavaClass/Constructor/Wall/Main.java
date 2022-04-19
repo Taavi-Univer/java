fd452e627869b6147925e7b0db618cf60c5480ec
Taavi-Univer <taavi.univer@gmail.com>
2022-04-19 15:55:46 +0300

    constructors

---
+++ JavaClass/Constructor/Wall/Main.java
@@ -0,0 +1,13 @@
+package JavaClass.Constructor.Wall;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Wall wall1 = new Wall();
+
+        System.out.println("Wall width: " + wall1.getWidth());
+        System.out.println("Wall height: " + wall1.getHeight());
+        System.out.println("Area: " + wall1.getArea());
+    }
+
+}

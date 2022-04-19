fa6eba1fcdab16a2ebc8496c48d7276c1dcbb07d
Taavi-Univer <taavi.univer@gmail.com>
2022-04-19 17:45:49 +0300

    update

--- JavaClass/Constructor/Wall/Main.java
+++ JavaClass/Constructor/Wall/Main.java
@@ -3,7 +3,7 @@ package JavaClass.Constructor.Wall;
 public class Main {
     public static void main(String[] args) {
 
-        Wall wall1 = new Wall();
+        Wall wall1 = new Wall(3, 6);
 
         System.out.println("Wall width: " + wall1.getWidth());
         System.out.println("Wall height: " + wall1.getHeight());

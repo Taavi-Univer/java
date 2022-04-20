bb18fc0cb05efe03929abbbb44ec5f53935f5c74
Taavi-Univer <taavi.univer@gmail.com>
2022-04-20 16:58:55 +0300

    two

---
+++ JavaClass/Inheritance/Circle/Circle.java
@@ -0,0 +1,23 @@
+package JavaClass.Inheritance.Circle;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle() {
+        this(0);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius < 0 ? 0 : radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return (radius * radius * Math.PI);
+    }
+
+}

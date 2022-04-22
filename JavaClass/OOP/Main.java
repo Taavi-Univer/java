69c7b02b46741e276750c236c0ec289e0855b6d5
Taavi-Univer <taavi.univer@gmail.com>
2022-04-22 18:54:45 +0300

    Hamburger

---
+++ JavaClass/OOP/Main.java
@@ -0,0 +1,18 @@
+package JavaClass.OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+        System.out.println("\n");
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.46);
+        healthyBurger.addHealthyAddition1("Lentils", 3.63);
+        System.out.println("Total Healty Burger price is " + healthyBurger.itemizeHamburger());
+    }
+}
